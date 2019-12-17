a1d7dd7f062b161f6ba46ebcc049ac241db63479
Scott Wey <scottwey@users.noreply.github.com>
2019-12-16 19:04:48 -0800

    rename package

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ NB: *Restart the Python session first* if you want to finetune on another datase
 
 
 setup(
-    name='gpt_2_simple',
-    packages=['gpt_2_simple'],  # this must be the same as the name above
+    name='gpt_2_simple_amp',
+    packages=['gpt_2_simple_amp'],  # this must be the same as the name above
     version='0.7',
     description="Python package to easily retrain OpenAI's GPT-2 " \
     "text-generating model on new texts.",
